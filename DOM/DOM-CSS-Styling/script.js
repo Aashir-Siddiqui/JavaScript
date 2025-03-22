bef1bbe500aeb26bc6a998182f39b46c3b03b758
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-03-22 22:05:28 +0500

    ClassList method practice

--- DOM/DOM-CSS-Styling/script.js
+++ DOM/DOM-CSS-Styling/script.js
@@ -19,3 +19,29 @@ document.querySelector("h3").classList.add("xyz")
 document.querySelector("h3").classList.remove("abc")
 c = document.querySelector("h3").classList
 console.log(c)
+
+
+// classList method toggle 
+
+document.querySelector("#BTN").addEventListener("click", changeColor)
+function changeColor() {
+    document.querySelector("#BTN").classList.toggle("btn")
+}
+
+
+// classList method length
+
+let getBtn1 = document.querySelector("#BTN").classList.length
+console.log(getBtn1)
+
+
+// classList method item
+
+let geth1 = document.querySelector("h1").classList.item(1)
+console.log(geth1)
+
+
+// classList method contains
+
+let getBtn2 = document.querySelector("#BTN").classList.contains("btn")
+console.log(getBtn2)
