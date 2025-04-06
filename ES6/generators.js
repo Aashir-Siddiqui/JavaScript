b4ac7353301f4e351cc83d25a2b6767f630a6c30
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-04-07 03:43:15 +0500

    Generators practice

--- ES6/generators.js
+++ ES6/generators.js
@@ -1,73 +1,73 @@
-// function *g(){
-//     yield "Yield No 1"
-//     yield "Yield No 2"
-//     yield "Yield No 3"
-// }
+function *g(){
+    yield "Yield No 1"
+    yield "Yield No 2"
+    yield "Yield No 3"
+}
 
-// console.log(g().next().value)
-// g().next()
-// console.log(g().next().value)
-
-
-// function *gen(){
-//     let nextNum = 1
-//     while(true){
-//         yield(nextNum++)
-//     }
-// }
-
-// let generator = gen()
-
-// for(let value of generator){
-//     if(value > 10) break
-//     console.log(value)
-// }
+console.log(g().next().value)
+g().next()
+console.log(g().next().value)
 
 
-// function *gen(){
-//     let result = (yield) * 10
-//     console.log(`Result: ${result}`)
-// }
+function *gen(){
+    let nextNum = 1
+    while(true){
+        yield(nextNum++)
+    }
+}
 
-// let g = gen()
-// g.next() //One time is ko chalana zarori hai
-// g.next(10)
+let generator = gen()
 
-
-// function *gen(){
-//     let result = [yield, yield, yield]
-//     console.log(`Result: ${result[1]}`)
-// }
-
-// let g = gen()
-// g.next() //One time is ko chalana zarori hai
-// g.next("Apple")
-// g.next("Mango")
-// g.next("Kiwi")
+for(let value of generator){
+    if(value > 10) break
+    console.log(value)
+}
 
 
-// function *gen(){
-//     yield 'HTML'
-//     yield* ['CSS', 'JS', 'React JS']
-// }
+function *gen(){
+    let result = (yield) * 10
+    console.log(`Result: ${result}`)
+}
 
-// let g = gen()
-// console.log(g.next().value)
-// console.log(g.next().value)
-// console.log(g.next().value)
-// console.log(g.next().value)
+let g = gen()
+g.next() //One time is ko chalana zarori hai
+g.next(10)
 
 
-// function *gen(){
-//     yield 'HTML'
-//     yield 'CSS'
-//     yield 'JS'
-//     yield 'React JS'
-// }
+function *gen(){
+    let result = [yield, yield, yield]
+    console.log(`Result: ${result[1]}`)
+}
 
-// let g = gen()
-// console.log(g.next().value)
-// console.log([...g])
+let g = gen()
+g.next() //One time is ko chalana zarori hai
+g.next("Apple")
+g.next("Mango")
+g.next("Kiwi")
+
+
+function *gen(){
+    yield 'HTML'
+    yield* ['CSS', 'JS', 'React JS']
+}
+
+let g = gen()
+console.log(g.next().value)
+console.log(g.next().value)
+console.log(g.next().value)
+console.log(g.next().value)
+
+
+function *gen(){
+    yield 'HTML'
+    yield 'CSS'
+    yield 'JS'
+    yield 'React JS'
+}
+
+let g = gen()
+console.log(g.next().value)
+console.log([...g])
 
 
 function *gen(){
