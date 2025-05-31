0de1682b732ff0897a040c8bf59741f7a1f693b5
Aashir Siddiqui <aashirsiddiqui217@gmail.com>
2025-05-31 05:42:07 +0500

    Practice deep copy

--- Advance_js/deepCopy_shallowCopy.js
+++ Advance_js/deepCopy_shallowCopy.js
@@ -1,6 +1,6 @@
 // shallowCopy hota hai jab ap kesi object ko copy karein with object assign ya spread k through lkn us k top props tu copy ho jate hn lkn nested object k props reference hi copy hote hn
 
-const obj = {
+const user = {
     name: "Aashir",
     age: 18,
     address: {
@@ -14,19 +14,40 @@ const obj = {
     }
 }
 
-// let obj2 = {...obj}
+// let obj2 = {...user}
 
 // obj2.address.houseNo = "120/55"
 
-// console.log("obj", obj.address.houseNo)
-// console.log("obj2", obj2.address.houseNo)
+// console.log("user:", user.address.houseNo)
+// console.log("obj2:", obj2.address.houseNo)
 
 
-// deepCopy first tareeka lkn recommended nahi hai use nahi karna
+// // deepCopy first tareeka lkn recommended nahi hai use nahi karna
 
-let obj2 = JSON.parse(JSON.stringify(obj))   //describe obj reference hai tu JSON.stringify is ko string main convert kare ga jis se ye primitive ban jae ga or JSON.pares is string ko wapis object main convert karde ga
+// let obj2 = JSON.parse(JSON.stringify(user))   //describe obj reference hai tu JSON.stringify is ko string main convert kare ga jis se ye primitive ban jae ga or JSON.pares is string ko wapis object main convert karde ga
 
-obj2.address.houseNo = "120/55"
+// obj2.address.houseNo = "120/55"
 
-console.log("obj", obj.address.houseNo)
-console.log("obj2", obj2.address.houseNo)
+// console.log("user:", user.address.houseNo)
+// console.log("obj2:", obj2.address.houseNo)
+
+// deepCopy second tareeka lkn recommended hai
+
+const makeDeepCopy = (obj) => {
+    if(typeof obj !== 'object' || obj === null) return obj;
+
+    var copiedVal = Array.isArray(obj) ? [] : {};
+    var keys = Object.keys(obj)
+
+    for(var i=0; i<keys.length; i++){
+        copiedVal[keys[i]] = makeDeepCopy(obj[keys[i]])
+    }
+
+    return copiedVal
+}
+
+let copy = makeDeepCopy(user)
+
+console.log("user:", user.socialLinks.facebook)
+console.log("copy:", copy.socialLinks.facebook = "Dev Aashir")
+console.log(copy)
